0990ceb8c2a5d7fe76944c8dc5f4c2af391b01d2
Eric Dobson <eric.n.dobson@gmail.com>
2016-01-11 20:34:37 -0800

    Change how primitives are defined so that adding types to them will be easier.

--- primitives.rkt
+++ primitives.rkt
@@ -13,10 +13,32 @@
   run-primitive)
 
 (begin-for-syntax
+
+  (define-syntax-class prim-ty
+    #:attributes (constructor)
+    (pattern (~datum Byte)
+      #:with constructor #'byte-val)
+    (pattern (~datum Bytes)
+      #:with constructor #'bytes-val)
+    (pattern (~datum Boolean)
+      #:with constructor #'boolean-val)
+    (pattern (~datum InputPort)
+      #:with constructor #'prim-port-val)
+    (pattern (~datum OutputPort)
+      #:with constructor #'prim-port-val)
+    ;; These should not be used for arg types
+    (pattern (~datum Void)
+      #:with constructor #'(λ (_) (void-val)))
+    (pattern (~datum Bottom)
+      #:with constructor #'(λ (x) x)))
+
+
+
   (define-syntax-class primitive-clause
     #:attributes (name match-clause)
-    (pattern ((name:id args:expr ...) body ...+)
-      #:with match-clause #'[(list 'name args ...) body ...])))
+    (pattern ((name:id (args:id (~datum :) types:prim-ty) ...) (~datum :) result-type:prim-ty body ...+)
+      #:with match-clause #'[(list 'name (types.constructor args) ...) 
+                             (result-type.constructor (let () body ...))])))
 
 
 (define-syntax define-primitives
@@ -29,58 +51,37 @@
            (match (cons name args)
              clauses.match-clause ...)))]))
 
-
-
 (define-primitives
   (supported-primitives run-primitive)
 
-  [(or (boolean-val x) (boolean-val y))
-   (boolean-val (or x y))]
-  [(and (boolean-val x) (boolean-val y))
-   (boolean-val (and x y))]
+  [(or [x : Boolean] (y : Boolean)) : Boolean (or x y)]
+  [(and [x : Boolean] (y : Boolean)) : Boolean (and x y)]
 
-  [(+ (byte-val x) (byte-val y))
-   (byte-val (+ x y))]
-  [(- (byte-val x) (byte-val y))
-   (byte-val (- x y))]
-  [(* (byte-val x) (byte-val y))
-   (byte-val (* x y))]
-  [(quotient (byte-val x) (byte-val y))
-   (byte-val (quotient x y))]
-  [(remainder (byte-val x) (byte-val y))
-   (byte-val (remainder x y))]
+  [(+ [x : Byte] (y : Byte)) : Byte (+ x y)]
+  [(- [x : Byte] (y : Byte)) : Byte (- x y)]
+  [(* [x : Byte] (y : Byte)) : Byte (* x y)]
+  [(quotient [x : Byte] (y : Byte)) : Byte (quotient x y)]
+  [(remainder [x : Byte] (y : Byte)) : Byte (remainder x y)]
 
-  [(= (byte-val x) (byte-val y))
-   (boolean-val (= x y))]
-  [(<= (byte-val x) (byte-val y))
-   (boolean-val (<= x y))]
-  [(< (byte-val x) (byte-val y))
-   (boolean-val (< x y))]
-  [(> (byte-val x) (byte-val y))
-   (boolean-val (> x y))]
-  [(>= (byte-val x) (byte-val y))
-   (boolean-val (>= x y))]
+  [(= [x : Byte] (y : Byte)) : Boolean (= x y)]
+  [(<= [x : Byte] (y : Byte)) : Boolean (<= x y)]
+  [(>= [x : Byte] (y : Byte)) : Boolean (>= x y)]
+  [(< [x : Byte] (y : Byte)) : Boolean (< x y)]
+  [(> [x : Byte] (y : Byte)) : Boolean (> x y)]
 
-  [(void)
-   (void-val)]
+  [(void) : Void (void)]
 
-  [(panic (bytes-val bytes))
-   (error-sentinal bytes)]
+  [(panic [bytes : Bytes]) : Bottom (error-sentinal bytes)]
 
-  [(make-bytes (byte-val size))
-   (bytes-val (make-bytes size))]
-  [(bytes-ref (bytes-val b) (byte-val index))
-   (byte-val (bytes-ref b index))]
-  [(bytes-set! (bytes-val b) (byte-val index) (byte-val v))
-   (bytes-set! b index v)
-   (void-val)]
-  [(bytes-length (bytes-val b))
-   (byte-val (bytes-length b))]
+  [(make-bytes [size : Byte]) : Bytes (make-bytes size)]
+  [(bytes-ref [b : Bytes] [index : Byte]) : Byte (bytes-ref b index)]
+  [(bytes-set! [b : Bytes] [index : Byte] [v : Byte]) : Void (bytes-set! b index v)]
+  [(bytes-length [b : Bytes]) : Byte (bytes-length b)]
 
-  [(write-bytes (bytes-val x) (prim-port-val p) (byte-val start-pos) (byte-val end-pos))
-   (byte-val (write-bytes x p start-pos end-pos))]
+  [(write-bytes [b : Bytes] [p : InputPort] [start-pos : Byte] [end-pos : Byte]) : Byte
+   (write-bytes b p start-pos end-pos)]
 
-  [(read-bytes (bytes-val b) (prim-port-val p) (byte-val start-pos) (byte-val end-pos))
+  [(read-bytes [b : Bytes] [p : InputPort] [start-pos : Byte] [end-pos : Byte]) : Byte
    (define amount-read (read-bytes! b p start-pos end-pos))
-   (byte-val (if (eof-object? amount-read) 0 amount-read))])
+   (if (eof-object? amount-read) 0 amount-read)])
 
