9ad9281954cab7c2d25b03393c6a364df86b0748
Eric Dobson <eric.n.dobson@gmail.com>
2016-05-17 20:29:16 -0700

    Remove unused machine-structs.

--- bootstrap/machine-structs.rkt
+++ bootstrap/machine-structs.rkt
@@ -2,31 +2,13 @@
 
 (provide (all-defined-out))
 
-(struct value () #:transparent)
-(struct function-val value (args env-box body) #:transparent)
-;(struct void-val value () #:transparent)
-;(struct byte-val value (v) #:transparent)
-;(struct boolean-val value (v) #:transparent)
-;(struct bytes-val value (v) #:transparent)
-;(struct box-val value (v) #:transparent)
-;(struct array-val value (v) #:transparent)
+(struct function-val (args env-box body) #:transparent)
 
 (struct error-sentinal (info) #:transparent)
 
-(struct prim-port-val value (port) #:transparent)
-(struct compiled-function-val value (impl) #:transparent)
-
 (struct variant-val (variant-name fields) #:transparent)
 (struct variant-constructor-val (variant-name fields) #:transparent)
-(struct field-accessor-val value (variant-name field-name) #:transparent)
-
-(struct halt-k () #:transparent)
-(struct apply-k (vals args env cont) #:transparent)
-(struct varargs-apply-k (vals args env cont) #:transparent)
-(struct if-k (true false env cont) #:transparent)
-(struct ignore-k (expr env cont) #:transparent)
-(struct bind-k (name expr env cont) #:transparent)
-(struct case-k (clauses env cont) #:transparent)
+(struct field-accessor-val (variant-name field-name) #:transparent)
 
 (struct full-name (module-name main-name) #:transparent)
 
