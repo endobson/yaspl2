e1be8c763a265b6c84a6201879bb33a16f8c1429
Eric Dobson <eric.n.dobson@gmail.com>
2017-06-24 14:04:43 -0700

    Make no uuids in bootstrap code and use correct bootstrap code.

--- bootstrap/setup-compiler.sh
+++ bootstrap/setup-compiler.sh
@@ -2,9 +2,9 @@
 set -e
 
 bazel build //bootstrap:compiler_binaries
-cp bazel-out/host/bin/bootstrap/bootstrap_library_compiler bootstrap/saved_library_compiler
+cp bazel-out/darwin_x86_64-fastbuild/bin/bootstrap/bootstrap_library_compiler bootstrap/saved_library_compiler
 chmod ug+w bootstrap/saved_library_compiler
-cp bazel-out/host/bin/bootstrap/bootstrap_main_stub bootstrap/saved_main_stub
+cp bazel-out/darwin_x86_64-fastbuild/bin/bootstrap/bootstrap_main_stub bootstrap/saved_main_stub
 chmod ug+w bootstrap/saved_main_stub
-cp bazel-out/host/bin/bootstrap/bootstrap_linker bootstrap/saved_linker
+cp bazel-out/darwin_x86_64-fastbuild/bin/bootstrap/bootstrap_linker bootstrap/saved_linker
 chmod ug+w bootstrap/saved_linker
