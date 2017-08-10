dbcb33f8b02c708c966b61c1de4fbc376458eb2b
Eric Dobson <eric.n.dobson@gmail.com>
2017-08-09 21:53:27 -0700

    Improve rewrite-imports.

--- rewrite-imports.sh
+++ rewrite-imports.sh
@@ -2,7 +2,11 @@
 set -e
 
 bazel build //tools:rewrite-imports
-for file in $(git st --porcelain | cut -c 4- | grep '^.*\.yaspl$')
+for file in $(git st --porcelain | cut -c 4- | sed -e "s/.* //" | grep '^.*\.yaspl$')
 do
-  bazel-bin/tools/rewrite-imports $file
+  exit_code=0
+  bazel-bin/tools/rewrite-imports $file || exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo "Error rewriting '$file'"
+  fi;
 done
