3d8bd17cdb1e33043af6014587545af2f1b8111f
Eric Dobson <eric.n.dobson@gmail.com>
2018-01-05 22:16:59 -0800

    Fix lint checking script and fix lint issue.

--- lint-check.sh
+++ lint-check.sh
@@ -3,4 +3,4 @@ set -e
 
 bazel build //tools/... //examples/... //libraries/... --aspects tools/yaspl-lint.bzl%yaspl_lint \
   --output_groups=lint
-find bazel-out/darwin_x86_64-fastbuild/bin/ -name '*.lint' -print0 | xargs -0 -- cat
+find `bazel info bazel-bin` -name '*.lint' -print0 | xargs -0 -- cat
