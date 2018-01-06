ce553c6aab20f4a862d37e6d953b32ffafcd2a9c
Eric Dobson <eric.n.dobson@gmail.com>
2018-01-06 11:08:45 -0800

    Make setup compiler work with new layout.

--- bootstrap/setup-compiler.sh
+++ bootstrap/setup-compiler.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
+BAZEL_OUT="$(bazel info output_path)"
 bazel build //bootstrap:compiler_binaries
-cp bazel-out/darwin_x86_64-fastbuild/bin/bootstrap/bootstrap_library_compiler bootstrap/saved_library_compiler
+cp "${BAZEL_OUT}"/host/bin/bootstrap/bootstrap_library_compiler bootstrap/saved_library_compiler
 chmod ug+w bootstrap/saved_library_compiler
-cp bazel-out/darwin_x86_64-fastbuild/bin/bootstrap/bootstrap_main_stub bootstrap/saved_main_stub
+cp "${BAZEL_OUT}"/host/bin/bootstrap/bootstrap_main_stub bootstrap/saved_main_stub
 chmod ug+w bootstrap/saved_main_stub
-cp bazel-out/darwin_x86_64-fastbuild/bin/bootstrap/bootstrap_linker bootstrap/saved_linker
+cp "${BAZEL_OUT}"/host/bin/bootstrap/bootstrap_linker bootstrap/saved_linker
 chmod ug+w bootstrap/saved_linker
