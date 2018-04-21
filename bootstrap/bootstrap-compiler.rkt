7042717902b8a4b14c63f136c0ee0997559ae13b
Eric Dobson <eric.n.dobson@gmail.com>
2018-04-21 09:43:29 -0700

    Make fill-stack be a thing.

--- bootstrap/bootstrap-compiler.rkt
+++ bootstrap/bootstrap-compiler.rkt
@@ -14,13 +14,16 @@
 (define-runtime-path src-root "..")
 
 (define (run-bootstrap-compiler file-list-file main-function)
-
-  (define source-files
-    (for/list ([file (in-list (file->bytes-lines file-list-file))])
-      (path->bytes (build-path src-root (bytes->path file)))))
   (define compiler-files
     (for/list ([file (in-list (file->bytes-lines compiler-list-file))])
       (build-path src-root (bytes->path file))))
+  (define source-files
+    (for/list ([file (in-list (file->bytes-lines file-list-file))])
+      (path->bytes (build-path src-root (bytes->path file)))))
+  (define prim-files
+    (list
+      (path->bytes
+        (build-path src-root "libraries/yaspl/runtime/fill-stack.prim"))))
 
   (define-values (modules signatures) (load-modules compiler-files))
 
@@ -30,7 +33,7 @@
 
 
   (let ([result (run-program modules signatures (module-name& '(compiler-main)) 'main #:stdin #""
-                             #:args (list* output-file main-function source-files))])
+                             #:args `(,output-file ,main-function ,@source-files ,@prim-files))])
     (write-bytes (program-result-stdout result) (current-output-port))
     (write-bytes (program-result-stderr result) (current-error-port))
     (when (program-result-error-info result)
