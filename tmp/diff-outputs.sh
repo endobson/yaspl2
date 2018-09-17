ded782314661756cbe8816225575f07f78d7612d
Eric Dobson <eric.n.dobson@gmail.com>
2018-09-16 21:28:52 -0700

    Update differ to include filename.

--- tmp/diff-outputs.sh
+++ tmp/diff-outputs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-diff output1.xxd prog1.xxd && \
-diff output2.xxd prog2.xxd && \
-diff output3.xxd prog3.xxd && \
-diff output4.xxd prog4.xxd && \
-diff output5.xxd prog5.xxd && \
-diff output6.xxd prog6.xxd
+diff -u output1.xxd prog1.xxd && \
+diff -u output2.xxd prog2.xxd && \
+diff -u output3.xxd prog3.xxd && \
+diff -u output4.xxd prog4.xxd && \
+diff -u output5.xxd prog5.xxd && \
+diff -u output6.xxd prog6.xxd
