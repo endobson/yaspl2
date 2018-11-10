c3a084e07157ec6502c1e29aa120874f9117428f
Eric Dobson <eric.n.dobson@gmail.com>
2018-11-10 09:34:36 -0800

    Strip object name from executable.

--- tmp/link-objects.sh
+++ tmp/link-objects.sh
@@ -2,5 +2,7 @@
 cd bazel-obj;
 for prog in `ls *.o  | sed -e 's/.o$//'`;
 do
-  ld "${prog}.o" -o "${prog}.exe" && xxd "${prog}.exe" >! "${prog}.xxd";
+  ld "${prog}.o" -o "${prog}.exe" && \
+  strip "${prog}.exe" -N "${prog}.o" && \
+  xxd "${prog}.exe" >! "${prog}.xxd";
 done;
