b7c7bfe8f72232324c098999702fd0f9cfa9125f
Eric Dobson <eric.n.dobson@gmail.com>
2018-11-12 21:49:59 -0800

    Make size change based on name and use lld.

--- tmp/link-objects.sh
+++ tmp/link-objects.sh
@@ -2,7 +2,9 @@
 cd bazel-obj;
 for prog in `ls *.o  | sed -e 's/.o$//'`;
 do
-  ld "${prog}.o" -o "${prog}.exe" && \
-  strip "${prog}.exe" -N "${prog}.o" && \
+  temp=$(mktemp)
+  ld.lld-6.0 "${prog}.o" -o "${temp}" --gc-sections && \
+  mv "${temp}" "${prog}.exe" && \
+  strip -D "${prog}.exe" -N "${prog}.o" && \
   xxd "${prog}.exe" >! "${prog}.xxd";
 done;
