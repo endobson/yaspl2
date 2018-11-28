15c9db702177199504daacde2438d8e9a637c0bf
Eric Dobson <eric.n.dobson@gmail.com>
2018-11-27 20:32:16 -0800

    Remove shell scripts for generating elf files.

--- tmp/regen-objects.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf tmp/bazel-obj && \
-mkdir tmp/bazel-obj && \
-bazel build //libraries/prim-language/examples/... --cpu=k8 && \
-cp -f bazel-bin/libraries/prim-language/examples/*.o tmp/bazel-obj && \
-rm -f tmp/bazel-obj/call1_lib.o
