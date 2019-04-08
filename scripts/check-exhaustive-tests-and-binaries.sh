6f64deaedc2932f45aab78a5a01e6b1ad4afd93e
Eric Dobson <eric.n.dobson@gmail.com>
2019-04-07 20:45:04 -0700

    Improve exhaustive check as we don't expect it to cover manual tests.

--- scripts/check-exhaustive-tests-and-binaries.sh
+++ scripts/check-exhaustive-tests-and-binaries.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-bazel query "(kind('yaspl_prim_binary', //...) - deps(//:all_binaries, 2))"
-bazel query "(kind('_test', //...) - tests(//:all_tests))"
+bazel query --noshow_progress \
+ "(kind('yaspl_prim_binary', //...) - deps(//:all_binaries, 2)) + "\
+ "(let all_test_targets = kind('_test', //...) in \$all_test_targets - "\
+    "attr(tags, manual, \$all_test_targets)  - tests(//:all_tests))"
