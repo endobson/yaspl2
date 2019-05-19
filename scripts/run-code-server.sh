028413e20e9e5a75e6077b3d65444b9a5883cb4e
Eric Dobson <eric.n.dobson@gmail.com>
2019-05-18 18:46:50 -0700

    Add check for number of arguments when running the code server.

--- scripts/run-code-server.sh
+++ scripts/run-code-server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo 1>&2 "Usage: $0 bazel-target"
+  exit 1
+fi
+
 bazel build libraries/code-http-server && \
 bazel-bin/libraries/code-http-server 127.0.0.1 8080 \
 `bazel query "filter('.src_dep', deps($1))" | \
