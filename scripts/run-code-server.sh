9b0830e052c1b3fcff60908b4cbedf176ecf3c79
Eric Dobson <eric.n.dobson@gmail.com>
2019-05-22 11:55:29 -0700

    Split code-http-server up into library pieces.

--- scripts/run-code-server.sh
+++ scripts/run-code-server.sh
@@ -5,8 +5,8 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-bazel build libraries/code-http-server && \
-bazel-bin/libraries/code-http-server 127.0.0.1 8080 \
+bazel build //libraries/code-http-server && \
+bazel-bin/libraries/code-http-server/code-http-server 127.0.0.1 8080 \
 `bazel query "filter('.src_dep', deps($1))" | \
    sed -e 's|^//||' -e 's|:|/|' -e 's|.src_dep$||' | \
    tail -r`
