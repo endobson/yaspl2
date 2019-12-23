869325c59b16a4e7dd0699c01269b88cafcdbcc6
Eric Dobson <eric.n.dobson@gmail.com>
2019-12-22 22:14:40 -0500

    Syntaxify eval statement.

--- bootstrap/compiler.rkt
+++ bootstrap/compiler.rkt
@@ -31,17 +31,17 @@
   (parameterize ([current-namespace ns])
     (namespace-require 'racket/base))
   (define main-fun
-    (eval
-      `(begin
-         (module prim racket/base
-           (provide (all-defined-out))
-           ,@(for/list ([(prim-name prim-val) (in-hash supported-primitives)])
-               `(define ,prim-name ,prim-val)))
-         ,@racket-modules
-         (require
-           (only-in ',(mod-name->racket-mod-name module-name)
-             [,main-name main-fun]))
-         main-fun)
+    (eval-syntax
+      #`(begin
+          (module prim racket/base
+            (provide (all-defined-out))
+            #,@(for/list ([(prim-name prim-val) (in-hash supported-primitives)])
+                `(define ,prim-name ,prim-val)))
+          #,@racket-modules
+          (require
+            (only-in '#,(mod-name->racket-mod-name module-name)
+              [#,main-name main-fun]))
+          main-fun)
       ns))
 
   (define process-args (cons #"/binary-path" supplied-args))
