0719929ab4f4fee9e100457851df12d2ae180488
Eric Dobson <eric.n.dobson@gmail.com>
2020-12-13 13:57:04 -0800

    Support wide range of dynamic blocks.

--- libraries/os/compress-disk-main.rkt
+++ libraries/os/compress-disk-main.rkt
@@ -22,7 +22,7 @@
 
   (when debug
     (displayln bits))
-  
+
   (define (peek-bits n)
     (list->string (take bits n)))
 
@@ -35,7 +35,7 @@
     (begin0
       (peek-bits n)
       (drop-bits! n)))
-  
+
   (define (fixed-code!)
     (match (get-bits! 2)
       ["11"
@@ -50,9 +50,10 @@
           (+ (string->number (string-append bs (get-bits! 3)) 2) 256)]
          [bs
           (+ (string->number (string-append bs (get-bits! 4)) 2) -48)])]))
-  
-  (define (fixed-distance!)
-    (match (string->number (get-bits! 5) 2)
+
+
+  (define (distance-code->distance code)
+    (match code
       [(? (lambda (x) (<= 0 x 3)) val)
        (add1 val)]
       [(? (lambda (x) (<= 4 x 29)) val)
@@ -63,48 +64,55 @@
           1
           (* odd (expt 2 n-bits))
           (string->number (reverse-string (get-bits! n-bits)) 2))]))
-  
+
+  (define (fixed-distance!)
+    (distance-code->distance (string->number (get-bits! 5) 2)))
+
   (struct literal (val) #:transparent)
   (struct repeat (length distance) #:transparent)
   (struct end () #:transparent)
-  
-  (define (fixed-code/struct!)
-    (match (fixed-code!)
+
+  (define (code->command code read-distance!)
+    (match code
       [(? (lambda (x) (<= 0 x 255)) val)
        (literal val)]
       [256
        (end)]
       [(? (lambda (x) (<= 257 x 264)) val)
        (define len (+ (* (- val 257) 1) 3))
-       (repeat len (fixed-distance!))]
+       (repeat len (read-distance!))]
       [(? (lambda (x) (<= 265 x 284)) val)
        (define-values (n-bits extra) (quotient/remainder (- val 261) 4))
        (define base-len (+ 3 (expt 2 (+ 2 n-bits)) (* extra (expt 2 n-bits))))
        (define len (+ base-len (string->number (reverse-string (get-bits! n-bits)) 2)))
        (when (= len 258)
          (error 'inflate "Invalid code length"))
-       (repeat len (fixed-distance!))]
-
+       (repeat len (read-distance!))]
       [285
-       (repeat 258 (fixed-distance!))]))
-  
-  
-  
+       (repeat 258 (read-distance!))]))
+
+  (define (fixed-code/struct!)
+    (code->command (fixed-code!) fixed-distance!))
+
+
   (unless (equal? (get-bits! 1) "1")
     (error 'inflate "Not last block"))
+
+  (define (read-all-codes read-code)
+    (let loop ()
+      (match (read-code)
+        [(end)
+         empty]
+        [code
+         (when debug
+           (displayln code))
+         (cons code (loop))])))
 
 
   (define output-commands
     (match (get-bits! 2)
       ["10"
-       (let loop ()
-         (match (fixed-code/struct!)
-           [(end)
-            empty]
-           [code
-            (when debug
-              (displayln code))
-            (cons code (loop))]))]
+       (read-all-codes fixed-code/struct!)]
       ["01"
        (define num-literals (string->number (reverse-string (get-bits! 5)) 2))
        (define num-distance-codes (string->number (reverse-string (get-bits! 5)) 2))
@@ -113,137 +121,154 @@
          (printf "Literals: 257 + ~a~n" num-literals)
          (printf "Distance codes: 1 + ~a~n" num-distance-codes)
          (printf "Code lengths: 4 + ~a~n" num-code-lengths))
+
+       (define (build-code-table code-vec)
+         ;; The current code in little endian order
+         (define current-code empty)
+         (define (ensure-length! len)
+           (define extra (- len (length current-code)))
+           (when (not (zero? extra))
+             (set! current-code (append (make-list extra #\0) current-code))))
+         (define (increment-code code)
+           (match code
+             [(list)
+              ;; If this happens the next code should not be used
+              (list)]
+             [(cons #\0 code)
+              (cons #\1 code)]
+             [(cons #\1 code)
+              (cons #\0 (increment-code code))]))
+         (define (increment-current-code!)
+           (begin0
+             (list->string (reverse current-code))
+             (set! current-code (increment-code current-code))))
+
+
+         (for*/list ([len (in-range (vector-length code-vec))]
+                     [code (sort (vector-ref code-vec len) <)])
+           (ensure-length! len)
+           (list (increment-current-code!) code)))
+
+       (define (read-using-code-table code-table)
+         (let/ec return
+           (for ([p (in-list code-table)])
+             (match p
+               [(list prefix code-num)
+                (define n (string-length prefix))
+                (when (equal? (peek-bits n) prefix)
+                  (drop-bits! n)
+                  (return code-num))]))
+           (error 'inflate "No prefix code matched")))
+
+
        (define code-length-codes
          (list 16 17 18 0 8 7 9 6 10 5 11 4 12 3 13 2 14 1 15))
+       (define meta-code-lengths (make-hash))
+
+
        (for ([i (+ 4 num-code-lengths)] [code code-length-codes])
          (define code-length (string->number (reverse-string (get-bits! 3)) 2))
-         (when (and debug (not (zero? code-length)))
-           (printf "Code ~a is length ~a~n" code code-length)))
+         (when (not (zero? code-length))
+           (hash-set! meta-code-lengths code code-length)
+           (when debug
+             (printf "Code ~a is length ~a~n" code code-length))))
+
+       (define max-meta-length
+         (apply max (hash-values meta-code-lengths)))
+
+
+       (define meta-code-vec (make-vector (add1 max-meta-length) empty))
+       (for ([(code-num length) meta-code-lengths])
+         (vector-set! meta-code-vec length
+                      (cons code-num (vector-ref meta-code-vec length))))
+       (define meta-code-table (build-code-table meta-code-vec))
+
 
        (define num-len-codes-read 0)
        (define (increase-num-len-codes! amt)
          (set! num-len-codes-read (+ num-len-codes-read amt)))
 
-       (define (read-len-code)
-         (cond
-           [(equal? (peek-bits 2) "00")
-            (drop-bits! 2)
-            (when debug
-              (printf "Code ~a is ~a~n" num-len-codes-read 0))
+       (define encoding-code-lengths (make-hash))
+       (define prev-code-len #f)
+
+       (define (process-len-code)
+         (match (read-using-code-table meta-code-table)
+           [(? (lambda (x) (<= 0 x 15)) len)
+            (unless (zero? len)
+              (hash-set! encoding-code-lengths num-len-codes-read len))
+            (set! prev-code-len len)
             (increase-num-len-codes! 1)]
-           [(equal? (peek-bits 3) "010")
-            (drop-bits! 3)
-            (when debug
-              (printf "Code ~a is ~a~n" num-len-codes-read 1))
-            (increase-num-len-codes! 1)]
-           [(equal? (peek-bits 3) "011")
-            (drop-bits! 3)
-            (when debug
-              (printf "Code ~a is ~a~n" num-len-codes-read 2))
-            (increase-num-len-codes! 1)]
-           [(equal? (peek-bits 3) "100")
-            (drop-bits! 3)
-            (when debug
-              (printf "Code ~a is ~a~n" num-len-codes-read 3))
-            (increase-num-len-codes! 1)]
-           [(equal? (peek-bits 3) "101")
-            (drop-bits! 3)
-            (when debug
-              (printf "Code ~a is ~a~n" num-len-codes-read 5))
-            (increase-num-len-codes! 1)]
-           [(equal? (peek-bits 3) "110")
-            (drop-bits! 3)
-            (when debug
-              (printf "Code is ~a~n" 18))
-            (define count (+ 11 (string->number (reverse-string (get-bits! 7)) 2)))
-            (when debug
-              (printf "Extra is ~a~n" count))
+           [16
+            (define count (+ 3 (string->number (reverse-string (get-bits! 2)) 2)))
+            (unless prev-code-len
+              (error 'inflate "Cannot refer to previous code length on first code."))
+            (unless (zero? prev-code-len)
+              (for ([i count])
+                (hash-set! encoding-code-lengths (+ num-len-codes-read i) prev-code-len)))
             (increase-num-len-codes! count)]
-           [(equal? (peek-bits 4) "1110")
-            (drop-bits! 4)
-            (when debug
-              (printf "Code ~a is ~a~n" num-len-codes-read 4))
-            (increase-num-len-codes! 1)]
-           [(equal? (peek-bits 4) "1111")
-            (drop-bits! 4)
-            (when debug
-              (printf "Code is ~a~n" 17))
+           [17
             (define count (+ 3 (string->number (reverse-string (get-bits! 3)) 2)))
-            (when debug
-              (printf "Extra is ~a~n" count))
+            (set! prev-code-len 0)
+            (increase-num-len-codes! count)]
+           [18
+            (define count (+ 11 (string->number (reverse-string (get-bits! 7)) 2)))
+            (set! prev-code-len 0)
             (increase-num-len-codes! count)]))
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (read-len-code)
-       (when debug
-         (printf "NumCodesRead ~a~n" num-len-codes-read))
+
+       (define expected-num-codes (+ 258 num-distance-codes num-literals))
+       (let loop ()
+         (when (< num-len-codes-read expected-num-codes)
+           (process-len-code)
+           (loop)))
+       (unless (= num-len-codes-read expected-num-codes)
+         (error 'inflate "Too many length codes ~a" num-len-codes-read))
+
+
+       (define max-litlen-length
+         (apply max
+           (for/list ([(code-num length) encoding-code-lengths]
+                      #:when (< code-num (+ 257 num-literals)))
+             length)))
+       (define max-distance-length
+         (apply max
+           (for/list ([(code-num length) encoding-code-lengths]
+                      #:when (>= code-num (+ 257 num-literals)))
+             length)))
+
+       (define litlen-code-vec (make-vector (add1 max-litlen-length) empty))
+       (for ([(code-num length) encoding-code-lengths]
+             #:when (< code-num (+ 257 num-literals)))
+         (vector-set! litlen-code-vec length
+                      (cons code-num (vector-ref litlen-code-vec length))))
+
+       (define distance-code-vec (make-vector (add1 max-distance-length) empty))
+       (for ([(code-num length) encoding-code-lengths]
+             #:when (>= code-num (+ 257 num-literals)))
+         (vector-set! distance-code-vec length
+                      (cons (- code-num (+ 257 num-literals))
+                            (vector-ref distance-code-vec length))))
+
+
+
+       (define litlen-code-table (build-code-table litlen-code-vec))
+       (define distance-code-table (build-code-table distance-code-vec))
 
        (define (read-distance)
-         (cond
-           [(equal? (peek-bits 1) "0")
-            (drop-bits! 1)
-            (define extra (string->number (reverse-string (get-bits! 2)) 2))
-            (+ 9 extra)]
-           [(equal? (peek-bits 1) "1")
-            (drop-bits! 1)
-            (define extra (string->number (reverse-string (get-bits! 3)) 2))
-            (+ 17 extra)]))
+         (distance-code->distance (read-using-code-table distance-code-table)))
 
-       (define (read-actual-code)
-         (cond
-           [(equal? (peek-bits 2) "00")
-            (drop-bits! 2)
-            (literal 97)]
-           [(equal? (peek-bits 2) "01")
-            (drop-bits! 2)
-            (literal 98)]
-           [(equal? (peek-bits 2) "10")
-            (drop-bits! 2)
-            (literal 99)]
-           [(equal? (peek-bits 3) "110")
-            (drop-bits! 3)
-            (literal 100)]
-           [(equal? (peek-bits 4) "1110")
-            (drop-bits! 4)
-            (end)]
-           [(equal? (peek-bits 5) "11110")
-            (drop-bits! 5)
-            (repeat 4 (read-distance))]
-           [(equal? (peek-bits 5) "11111")
-            (drop-bits! 5)
-            (repeat 7 (read-distance))]))
+       (define (read-actual-command)
+         (code->command (read-using-code-table litlen-code-table) read-distance))
 
-       (let loop ()
-         (match (read-actual-code)
-           [(end)
-            empty]
-           [code
-            (when debug
-              (displayln code))
-            (cons code (loop))])) ]))
+       (read-all-codes read-actual-command)]))
 
   (when debug
     (displayln "Commands:")
     (for ([command output-commands])
       (displayln command)))
-  
+
   (define output-buffer (make-bytes (expt 2 15)))
   (define current-output-pos 0)
-  
+
   (for ([output-command output-commands])
     (match output-command
       [(literal val)
@@ -256,9 +281,9 @@
          (set! current-output-pos (add1 current-output-pos)))]))
   (subbytes output-buffer 0 current-output-pos))
 
-     
+
 (define (deflate-bytes input)
-  (call-with-output-bytes 
+  (call-with-output-bytes
     (lambda (out)
       (call-with-input-bytes input
         (lambda (in)
@@ -299,7 +324,7 @@
     (for/list ([i (in-range amount)])
       (single-random length alphabet-size))))
 
-  
+
 (define test-inputs
   (append
     ;full-exhaustive-length-zero
@@ -307,18 +332,17 @@
     ;full-exhaustive-length-two
     ;exhaustive-length-10-alphabet-2
     ;(many-random #:amount 1000 #:length 17 #:alphabet-size 5)
-    ;(many-random #:amount 1000 #:length 16 #:alphabet-size 4)
     ;(many-random #:amount 5000 #:length 30 #:alphabet-size 26)
+    ;(many-random #:amount 5000 #:length 100 #:alphabet-size 5)
+
 
     (list
-      #"aababcbacdbcacbcdaababcbaaccddbbccaaccbbd")
-    ;(many-random #:amount 1000 #:length 17 #:alphabet-size 5)
-
-    ;(list
-    ;  (make-bytes 76 65)
-    ;  (make-bytes 200 65)
-    ;  #"AAAAABAAAA"
-    ;  #"EEDACCBCCDCDDAC")
+      (make-bytes 76 65)
+      (make-bytes 200 65)
+      #"AAAAABAAAA"
+      #"EEDACCBCCDCDDAC"
+      #"aababcbacdbcacbcdaababcbaaccddbbccaaccbbd"
+      #"aababcbabccdacdbcacbcdaabbbbbbaacdbcbaaccddbbccaaccbbd")
 
     ))
 
