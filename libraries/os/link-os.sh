b33b6aa88095fe2aadbf8e0eebaab6292c8c4954
Eric Dobson <eric.n.dobson@gmail.com>
2021-01-30 17:32:56 -0800

    Add script for linking kernel.

---
+++ libraries/os/link-os.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+bazel build //libraries/os:{examples,msabi-runtime.o} \
+  --platforms=//constraints/platforms:yasplos_x86_64
+
+cp -f bazel-bin/libraries/os/examples.o tmp/os/examples.obj
+cp -f bazel-bin/libraries/os/msabi-runtime.o tmp/os/msabi-runtime.obj
+
+bazel build //libraries:linker 
+
+rm -f tmp/kernel.efi
+bazel-bin/libraries/linker yasplos tmp/kernel.efi \
+  tmp/os/{hello-c,primitives,serial,strings,efi_util,descriptor_tables,scheduler,examples,msabi-runtime}.obj 
+
